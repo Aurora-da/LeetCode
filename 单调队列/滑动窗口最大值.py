0f65709761dd8bae7c6d703ddf228a9e2cc924d1
aurora-da <1952832816@qq.com>
2026-08-01 12:39:36 +0800

    增加注释

--- 单调队列/滑动窗口最大值.py
+++ 单调队列/滑动窗口最大值.py
@@ -6,17 +6,22 @@ class Solution:
         n = len(nums)
         q = collections.deque()
         for i in range(k):
+            # 构造一个递减的单调队列，保证所有可能的结果都在队列中
             while q and nums[i]>=nums[q[-1]]:
                 q.pop()
             q.append(i)
 
         ans = [nums[q[0]]]
         for i in range(k, n):
+            # 如果新元素大于队列后面的那些，则说明那些元素不是这段区间的最大值，直接弹出即可
             while q and nums[i]>=nums[q[-1]]:
                 q.pop()
+            # 将新元素的下标放入队列中
             q.append(i)
+            # 将不在区间的元素全部弹出
             while q[0]<=i-k:
                 q.popleft()
+            # 队列的首个下标就是当前区间的最大值
             ans.append(nums[q[0]])
 
         return ans
