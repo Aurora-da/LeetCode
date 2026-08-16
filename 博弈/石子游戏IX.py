a4d7ee63e712437a3145740af89cf7c4c3ec790e
aurora-da <1952832816@qq.com>
2026-08-16 12:29:50 +0800

    博弈

---
+++ 博弈/石子游戏IX.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt0 = cnt1 = cnt2 = 0
+        for stone in stones:
+            if stone % 3 == 0:
+                cnt0 += 1
+            elif stone % 3 == 1:
+                cnt1 += 1
+            else:
+                cnt2 += 1
+        if cnt0 % 2 == 0:
+            return cnt1 >= 1 and cnt2 >= 1
+        return cnt1 - cnt2 > 2 or cnt2 -cnt1 > 2
