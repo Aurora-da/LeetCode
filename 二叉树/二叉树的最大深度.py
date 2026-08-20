42572c3bd5a1336eee0543985b1218fffd54d0d7
aurora-da <1952832816@qq.com>
2026-08-20 12:03:38 +0800

    新增广度优先遍历方法

--- 二叉树/二叉树的最大深度.py
+++ 二叉树/二叉树的最大深度.py
@@ -9,6 +9,7 @@ class TreeNode:
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         def get_depth(root: TreeNode) -> int:
+            # 深度优先遍历
             if root is None:
                 return 0
             left_depth = get_depth(root.left)
@@ -18,3 +19,29 @@ class Solution:
         return get_depth(root)
 
 
+"""
+广度优先遍历
+from collections import deque
+from typing import Optional
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        q = deque([root])
+        ans = 0
+        
+        while q:  # 当队列不为空时继续
+            ans += 1
+            # 处理当前层的所有节点
+            for _ in range(len(q)):
+                node = q.popleft()
+                
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        
+        return ans
+"""
