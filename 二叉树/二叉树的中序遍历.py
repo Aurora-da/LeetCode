890fe5c8e31159d47822bb12f9c92faa9f904901
aurora-da <1952832816@qq.com>
2026-08-20 11:40:29 +0800

    二叉树

---
+++ 二叉树/二叉树的中序遍历.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+
+        def helper(root: TreeNode):
+            if not root:
+                return
+
+            if root.left:
+                helper(root.left)
+
+            res.append(root.val)
+
+            if root.right:
+                helper(root.right)
+
+        helper(root)
+
+        return res
