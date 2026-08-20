6667a7e8399de16c62e29f9f6f115daf430b592e
aurora-da <1952832816@qq.com>
2026-08-20 11:52:27 +0800

    二叉树

---
+++ 二叉树/二叉树的最大深度.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        def get_depth(root: TreeNode) -> int:
+            if root is None:
+                return 0
+            left_depth = get_depth(root.left)
+            right_depth = get_depth(root.right)
+            return max(left_depth, right_depth) + 1
+
+        return get_depth(root)
+
+
